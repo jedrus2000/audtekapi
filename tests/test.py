bd3ccd89e45bded1ec83026520d7b172aeab66a6
Andrzej Bargański <a.barganski@gmail.com>
2019-01-06 12:03:41 +0100

    Some basic tests.

---
+++ tests/test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import unittest
+import logging
+from datetime import datetime
+
+import audtekapi as api
+
+logger = logging.getLogger(__name__)
+
+
+class AudtekAPI(unittest.TestCase):
+    def setUp(self):
+        logger.setLevel('DEBUG')
+        pass
+
+    def test_password_hashing(self):
+        self.assertEqual(
+            api._get_hashed_password('myPasswordAtAudioteka', '3660123456'),
+            'DA290D407F60AB87DD44A58A5DD02F7B7AF8D3B46547CF687D674380D18180DB3EA432BE')
+
+    def test_epoch_to_datetime(self):
+        self.assertEqual(
+            api.epoch_to_datetime('/Date(1545693401480+0100)/'),
+            datetime(year=2018, month=12, day=24, hour=23, minute=16, second=41, microsecond=480000))
+
